7ee03ee1e155b42b3fc7181fe33e5460fa09160c
Matti Paksula <matti.paksula@iki.fi>
2020-03-04 02:57:23 +0200

    :beers:

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ locals {
 }
 
 module "files" {
-  source = "git::https://github.com/matti/terraform-shell-resource.git?ref=v1.0.5"
+  source = "git::https://github.com/matti/terraform-shell-resource.git?ref=v1.0.7"
   #source  = "../terraform-shell-resource"
   command = "ls -l"
 }
